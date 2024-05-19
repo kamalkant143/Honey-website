0fe287bd4757e1a99879106f73bcae40a09f8fed
kamal kant <kamalshakya14311@gmail.com>
2024-05-19 17:29:14 +0530

    media

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const mobile = document.querySelector(".mobile")
+const navBar = document.querySelector(".nav-bar")
+
+
+mobile.addEventListener("click",function(){
+        navBar.classList.toggle("open")
+        mobile.classList.toggle("fa-xmark")
+
+})
+
+
+window.addEventListener("scroll",function(){
+    navBar.classList.remove("open")
+    mobile.classList.remove("fa-xmark")
+
+
+})
+
+
+
+
+
+
+
+
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('#header');
     if (window.scrollY > 80) { 
