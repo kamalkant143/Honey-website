eae3792f0b5cd8ff290686900e7abc4eb608e1ab
kamal kant <kamalshakya14311@gmail.com>
2024-05-19 13:40:58 +0530

    media

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('#header');
-    if (window.scrollY > 50) { 
+    if (window.scrollY > 80) { 
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
